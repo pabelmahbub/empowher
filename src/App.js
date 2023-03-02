3fdd65748c7823e7ba0de58d3b1e8c71efd4b25a
apple <apple@appurunoMacBook-Pro.local>
2023-03-03 05:40:14 +0900

    google analytics set

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
 import {router} from '../src/Routes/Routes'
 import {RouterProvider } from 'react-router-dom';
+import ReactGA from 'react-ga';
 
 function App() {
+
+  const TRACKING_ID = "G-YNXVWYCYPZ"; // OUR_TRACKING_ID
+  ReactGA.initialize(TRACKING_ID);
   
   return (
     <div className='max-w-7xl mx-auto'>
