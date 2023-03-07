70ce47b68ebcbe3625ebe2ebcdef937b07a2f8ac
apple <apple@appurunoMacBook-Pro.local>
2023-03-07 22:40:36 +0900

    delay component display created

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import {router} from '../src/Routes/Routes'
 import {RouterProvider } from 'react-router-dom';
 import ReactGA from 'react-ga';
+import Contribute from './Pages/Home/Contribute';
 
 
 
 function App() {
+
+
+
   ReactGA.initialize('G-YNXVWYCYPZ');
 
   return (
     <div className='max-w-7xl mx-auto'>
       <RouterProvider router={router} />
+      
     </div>
   );
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
 import Banner from './Banner';
@@ -6,15 +6,30 @@ import StepOne from './StepOne';
 import Services from './Services';
 import ImagesCards from './ImagesCards';
 import LastBanner from './LastBanner';
+import Contribute from './Contribute';
 
 function Home() {
+  const [showComponent, setShowComponent] = useState(false);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowComponent(true);
+    }, 8000);
+  
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div>
+       
       <Banner />
       <StepOne />
       <Services />
       <ImagesCards />
+      <div style={{position:''}}>
+         {showComponent && <Contribute />}
+      </div>
       <LastBanner />
+      
     </div>
   )
 }
