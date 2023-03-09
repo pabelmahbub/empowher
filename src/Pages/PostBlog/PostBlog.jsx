54cc0fde0498afbb593e72827fc71da9c8f2b1ec
apple <apple@appurunoMacBook-Pro.local>
2023-03-10 05:06:42 +0900

    post blog css fix

--- src/Pages/PostBlog/PostBlog.jsx
+++ src/Pages/PostBlog/PostBlog.jsx
@@ -34,20 +34,20 @@ function PostBlog() {
       <h1 className='bottomText mb-6'><span className='title'>13 Questions: 1 minute <br/>Suggest/Advise To your younger one</span></h1>
          {/**React-hook-form */}
       <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("Date", { required: true, maxLength: 100 })} placeholder='Date*' />
-      <input {...register("Name", { required: true, maxLength: 50 })} placeholder='Name/ Pseudo Name*'/>
-      <input {...register("Location", { required: true, maxLength: 500 })} placeholder='Your Location*'/>
-      <input {...register("Old", { required: true, maxLength: 500 })} placeholder='Age*'/>
-      <input {...register("Education", {  required: true, maxLength: 300 })} placeholder="Educated female child(percentage) around you?*"/>
-      <input {...register("Fields", {  required: true, maxLength: 300 })} placeholder="Reasons women are not 50/50 in all fields?"/>
-      <input {...register("Life", {  required: true, maxLength: 300 })} placeholder="Must needed in women's life/profession?*"/>
-      <input {...register("Feel", { required: true, maxLength: 500 })} placeholder='What is your feelings about life?*'/>
-      <input {...register("Society", { required: true, maxLength: 500 })} placeholder='what is under appreciated in our society?*'/>
-      <input {...register("Unhappy", { required: true, maxLength: 500 })} placeholder='Why people are unhappy in surroundings?*'/>
-      <input {...register("Advice", { required: true, maxLength: 500 })} placeholder='Advice to anyone of your age/ junior*'/>
-      <input {...register("Change", { required: true, maxLength: 300 })} placeholder="what do you want to change in yourself?*"/>
-      <input {...register("Title", { required: true, maxLength: 40 })} placeholder='Title of the interview?*'/>
-      <input {...register("Image", {  maxLength: 1000 })} placeholder="Image URL"/>
+      <input {...register("Date", { required: true, maxLength: 100 })} className="text-xs" placeholder='Date*' />
+      <input {...register("Name", { required: true, maxLength: 50 })} className="text-xs" placeholder='Name/ Pseudo Name*'/>
+      <input {...register("Location", { required: true, maxLength: 500 })} className="text-xs" placeholder='Your Location*'/>
+      <input {...register("Old", { required: true, maxLength: 500 })} className="text-xs" placeholder='Age*'/>
+      <input {...register("Education", {  required: true, maxLength: 300 })} className="text-xs" placeholder="Educated female child(percentage) around you?*"/>
+      <input {...register("Fields", {  required: true, maxLength: 300 })} className="text-xs" placeholder="Reasons women are not 50/50 in all fields?"/>
+      <input {...register("Life", {  required: true, maxLength: 300 })} className="text-xs" placeholder="Must needed in women's life/profession?*"/>
+      <input {...register("Feel", { required: true, maxLength: 500 })} className="text-xs" placeholder='What is your feelings about life?*'/>
+      <input {...register("Society", { required: true, maxLength: 500 })} className="text-xs" placeholder='what is under appreciated in our society?*'/>
+      <input {...register("Unhappy", { required: true, maxLength: 500 })} className="text-xs" placeholder='Why people are unhappy in surroundings?*'/>
+      <input {...register("Advice", { required: true, maxLength: 500 })} className="text-xs" placeholder='Advice to anyone of your age/ junior*'/>
+      <input {...register("Change", { required: true, maxLength: 300 })} className="text-xs" placeholder="what do you want to change in yourself?*"/>
+      <input {...register("Title", { required: true, maxLength: 40 })} className="text-xs" placeholder='Title of the interview?*'/>
+      <input {...register("Image", {  maxLength: 1000 })} className="text-xs" placeholder="Image URL"/>
       {/* <input {...register("Initially", {required: true, maxLength: 70 })} placeholder="Initially what happened?[about 100 words] *"/>
       <textarea {...register("Story")} placeholder='My Story[1000 words]'/>
       <input {...register("Image", {  maxLength: 1000 })} placeholder="Image URL"/> */}
